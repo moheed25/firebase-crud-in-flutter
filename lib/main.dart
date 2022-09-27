635c8229692267548670ef52d3d907aee8818fb8
unknown <moheed25@gmail.com>
2022-09-27 16:03:58 +0500

    work complete

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crud/firebase_options.dart';
+import 'package:firebase_crud/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        // CHeck for Errors
+        if (snapshot.hasError) {
+          print("Something went Wrong");
+        }
+        // once Completed, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Flutter Firestore CRUD',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            debugShowCheckedModeBanner: false,
+            home: HomePage(),
+          );
+        }
+        return CircularProgressIndicator();
+      },
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_crud/pages/add_student_page.dart';
+import 'package:firebase_crud/pages/list_student_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Flutter FireStore CRUD'),
+            ElevatedButton(
+              onPressed: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AddStudentPage(),
+                  ),
+                )
+              },
+              child: Text('Add', style: TextStyle(fontSize: 20.0)),
+              style: ElevatedButton.styleFrom(primary: Colors.deepPurple),
+            )
+          ],
+        ),
+      ),
+      body: ListStudentPage(),
+    );
+  }
+}
